91ae129133cd18d8d6d3266709644a9fceb0c4cf
dakshatasmetkari <dakshatasmetkari@gmail.com>
2022-11-12 18:38:15 +0530

    commit 4

--- genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
+++ genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GenPdfApplication {
 
 	public static void main(String[] args) {
-		System.out.println("gen pdf");
+		System.out.println("gen pdf 2");
 		SpringApplication.run(GenPdfApplication.class, args);
 		
 		System.out.println("gen pdf ended");
