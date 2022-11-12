dca7f906fa0617d9ba790c29df29164ce2019e17
dakshatasmetkari <dakshatasmetkari@gmail.com>
2022-11-12 18:32:51 +0530

    commit 1

--- genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
+++ genPdf/src/main/java/com/demo/pdf/genPdf/GenPdfApplication.java
@@ -9,6 +9,7 @@ public class GenPdfApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GenPdfApplication.class, args);
 		System.out.println("gen pdf started");
+		System.out.println("gen pdf ended");
 	}
 
 }
